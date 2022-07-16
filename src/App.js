6f78ec2423038796c91796a9faf9bc1a17b36389
dmi3y-v <dmi3y.vagin@yandex.ru>
2022-07-16 16:17:20 +0300

    posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-import Counter from "./components/Counter";
-import Input from "./components/Input";
+import React, { useState } from "react";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
+import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript 1", body: "Description 1" },
+    { id: 2, title: "JavaScript 2", body: "Description 2" },
+    { id: 3, title: "JavaScript 3", body: "Description 3" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", body: "Description 1" },
+    { id: 2, title: "Python 2", body: "Description 2" },
+    { id: 3, title: "Python 3", body: "Description 3" },
+  ]);
+
   return (
     <div className="App">
-      <Counter />
-      <Input />
+      <PostList posts={posts} title="Посты про JS" />
+      <PostList posts={posts2} title="Посты про Python" />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PostItem = function (props) {
+  return (
+    <div className="post">
+      <div className="post_content">
+        <strong>
+          {props.post.id}. {props.post.title}
+        </strong>
+        <div>{props.post.body}</div>
+      </div>
+      <div className="post_btn">
+        <button>Удалить</button>{" "}
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
