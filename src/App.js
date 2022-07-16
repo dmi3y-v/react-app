32921d3f7d5e64679b3fded56884695e4103ca27
dmi3y-v <dmi3y.vagin@yandex.ru>
2022-07-16 14:44:03 +0300

    Counter + input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState("Текст в инпуте");
 
   function increment() {
     setCounter(counter + 1);
@@ -14,6 +15,12 @@ function App() {
   return (
     <div className="App">
       <h1>{counter}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      ></input>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
